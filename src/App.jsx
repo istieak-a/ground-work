d320a80abae634e4998dbfac9f8c0644062d48c7
Istieak Ahmed <istieak.ahmed@yahoo.com>
2024-09-07 01:10:22 +0600

    Optimize package dependencies and add smooth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useRef } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
+import { LocomotiveScrollProvider } from 'react-locomotive-scroll';
+import 'locomotive-scroll/dist/locomotive-scroll.css';
 import Header from './Components/Header/Header'; 
 import Homepage from './Pages/Homepage/Homepage'; 
 import Footer from './Components/Footer/Footer';
@@ -8,18 +10,31 @@ import SocialMedia from './Pages/SocialMedia/SocialMedia';
 import Cgi from './Pages/Cgi/Cgi';
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path='/website-projects' element={<Websites />} />
-        <Route path='/social-media' element={<SocialMedia />} />
-        <Route path='/cgi' element={<Cgi />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-  )
-}
+  const scrollRef = useRef(null);
 
-export default App
+  return (
+    <LocomotiveScrollProvider
+      options={{
+        smooth: true,
+        // Additional options if needed
+      }}
+      watch={[]}
+      containerRef={scrollRef}
+    >
+      <div data-scroll-container ref={scrollRef}>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path='/website-projects' element={<Websites />} />
+            <Route path='/social-media' element={<SocialMedia />} />
+            <Route path='/cgi' element={<Cgi />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </div>
+    </LocomotiveScrollProvider>
+  );
+};
+
+export default App;
