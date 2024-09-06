e122f9ac9764db00851565493c42c39a06c2cc66
Istieak Ahmed <istieak.ahmed@yahoo.com>
2024-09-06 22:48:53 +0600

    Refactor header component and optimize mobile navigation
    - Add state and toggle function for mobile navigation
    - Close mobile navigation after link is clicked
    - Improve layout and styles for header component
    - Add mobile navigation with recent works links
    - Update CSS for carousel item and image
    - Add mobile navigation styles

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,22 +1,89 @@
-import React from 'react'; 
-import { RxHamburgerMenu } from "react-icons/rx";
+import React, { useState } from "react";
+import { RxHamburgerMenu, RxCross1 } from "react-icons/rx";
 import logo from "../../assets/logo.png";
-import { Link } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return (
-    <div className='bg-black flex md:flex-col md:gap-5 items-center justify-between py-6 px-5'>
-        <h1 className='md:hidden'></h1>
-        <Link to={'/'}><img className='w-[110px]' src={logo} alt="logo" /></Link>
-        <div className='text-white gap-8 text-[16px] font-sans hidden md:flex'>
-          <Link to={'/website-projects'} className='hover:text-[#78b6b2] duration-300'>websites</Link>
-          <Link to={'/cgi'} className='hover:text-[#78b6b2] duration-300'>CGI/VFX</Link>
-          <Link to={'/social-media'} className='hover:text-[#78b6b2] duration-300'>social media</Link>
-        </div>
-        <button className='text-white text-2xl md:hidden'><RxHamburgerMenu /></button>
-    </div>
-  )
-}
+  const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
 
-export default Header
+  const toggleMobileNav = () => {
+    setIsMobileNavOpen(!isMobileNavOpen);
+  };
+
+  const handleLinkClick = () => {
+    setIsMobileNavOpen(false); // Close the mobile nav after a link is clicked
+  };
+
+  return (
+    <div className="flex items-center justify-between bg-black px-5 py-6 md:flex-col md:gap-5">
+      <h1 className="md:hidden"></h1>
+      <Link to={"/"}>
+        <img className="w-[110px]" src={logo} alt="logo" />
+      </Link>
+      <div className="hidden gap-8 font-sans text-[16px] text-white md:flex">
+        <Link
+          to={"/website-projects"}
+          className="duration-300 hover:text-[#78b6b2]"
+        >
+          websites
+        </Link>
+        <Link to={"/cgi"} className="duration-300 hover:text-[#78b6b2]">
+          CGI/VFX
+        </Link>
+        <Link
+          to={"/social-media"}
+          className="duration-300 hover:text-[#78b6b2]"
+        >
+          social media
+        </Link>
+      </div>
+
+      <button
+        className="text-2xl text-white md:hidden"
+        onClick={toggleMobileNav}
+      >
+        <RxHamburgerMenu />
+      </button>
+
+      {/* Mobile Navigation */}
+      <div className={`mobile-nav ${isMobileNavOpen ? "open" : ""}`}>
+        <button
+          className="close-btn text-2xl text-white"
+          onClick={toggleMobileNav}
+        >
+          <RxCross1 />
+        </button>
+        <div className="flex flex-col gap-10">
+          <h1 className="line-before text-[24px] font-semibold text-white">
+            our recent <span className="text-primary">works</span>
+          </h1>
+          <div className="flex flex-col gap-3 text-[15px] text-white">
+            <Link
+              to={"/website-projects"}
+              onClick={handleLinkClick}
+              className="duration-300 hover:text-[#78b6b2]"
+            >
+              websites
+            </Link>
+            <Link
+              to={"/cgi"}
+              onClick={handleLinkClick}
+              className="duration-300 hover:text-[#78b6b2]"
+            >
+              CGI/VFX
+            </Link>
+            <Link
+              to={"/social-media"}
+              onClick={handleLinkClick}
+              className="duration-300 hover:text-[#78b6b2]"
+            >
+              social media
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
